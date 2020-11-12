cfbcdc3e4186ce5b14eaf8c7453d3c97efa2397c
avkucher <kuch.art@mail.ru>
2020-11-12 19:56:09 +0500

    init commit

--- main.py
+++ main.py
@@ -1,72 +1,50 @@
-class AhoNode:
-    ''' Вспомогательный класс для построения дерева
-    '''
-    def __init__(self):
-        self.goto = {}
-        self.out = []
-        self.fail = None
+from random import randint
+
+UZ = [
+    [],
+    ['K1', 'K1', 'K1'],
+    ['K2', 'K2', 'K1'],
+    ['K3', 'K3', 'K2']
+]
+
+def damage(p, d, s = 0, e = 100):
+    k = randint(s, e)
+    while p != None and (p - d > k or p + d < k):
+        k = randint(s, e)
+    return k
 
 
-def aho_create_forest(patterns):
-    '''Создать бор - дерево паттернов
-    '''
-    root = AhoNode()
+print('k experts')
+k = int(input())
 
-    for path in patterns:
-        node = root
-        for symbol in path:
-            node = node.goto.setdefault(symbol, AhoNode())
-        node.out.append(path)
-    return root
+print('Size IS(0=object, 1=region, 2=federation)')
+s = int(input())
 
+print('count experiments')
+e = int(input())
 
-def aho_create_statemachine(patterns):
-    # Создаем бор, инициализируем
-    # непосредственных потомков корневого узла
-    root = aho_create_forest(patterns)
-    queue = []
-    for node in root.goto.values():
-        queue.append(node)
-        node.fail = root
+d = 15
 
-    # Инициализируем остальные узлы:
-    # 1. Берем очередной узел (важно, что проход в ширину)
-    # 2. Находим самую длинную суффиксную ссылку для этой вершины - это и будет fail-функция
-    # 3. Если таковой не нашлось - устанавливаем fail-функцию в корневой узел
-    while len(queue) > 0:
-        rnode = queue.pop(0)
+experiments = []
+for i in range(e):
+    ex = []
+    for j in range(k):
+        ek = damage(experiments[i - 1][j] if i else None, d)
+        ex.append(ek)
+    exa = sum(ex) // len(ex)
+    experiments.append(ex)
+    experiments[i].append(exa)
 
-        for key, unode in rnode.goto.items():
-            queue.append(unode)
-            fnode = rnode.fail
-            while fnode is not None and key not in fnode.goto:
-                fnode = fnode.fail
-            unode.fail = fnode.goto[key] if fnode else root
-            unode.out += unode.fail.out
+[print(l) for l in experiments]
 
-    return root
+expl = experiments[e-1][k]
 
+uz = 0
+if expl > 70:
+    uz = 1
+elif 20 < expl and expl <= 70:
+    uz = 2
+elif expl <= 20:
+    uz = 3
 
-def aho_find_all(s, root, callback):
-    '''Находит все возможные подстроки из набора паттернов в строке.
-    '''
-    node = root
-
-    for i in range(len(s)):
-        while node is not None and s[i] not in node.goto:
-            node = node.fail
-        if node is None:
-            node = root
-            continue
-        node = node.goto[s[i]]
-        for pattern in node.out:
-            callback(i - len(pattern) + 1, pattern)
-
-
-def on_occurence(pos, patterns):
-    print(f"At pos {pos} found pattern:{patterns}")
-
-patterns = ['a', 'ab', 'abc', 'bc', 'c', 'cba', 'qwe']
-s = "abcba"
-root = aho_create_statemachine(patterns)
-aho_find_all(s, root, on_occurence)
+print(UZ[uz][s])
