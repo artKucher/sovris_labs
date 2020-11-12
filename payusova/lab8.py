b035c812c7c50d4950d4027ff0f57ffb7cc0aff0
Dmitry Sviridenko <sviriden0001@gmail.com>
2020-11-12 20:15:10 +0500

    realise 8 lab

---
+++ payusova/lab8.py
@@ -0,0 +1,39 @@
+from random import randint
+import numpy as np
+
+
+class PageRank:
+    page_count = None
+    iterations = None
+
+    def __init__(self, page_count=4, iterations=5):
+        self.page_count = page_count
+        self.iterations = iterations
+        self.init_vector = self.get_vector()
+        self.init_matrix = self.get_matrix()
+
+    def probability(self):
+        try:
+            return round(1 / randint(0, self.page_count - 1), 2)
+        except ZeroDivisionError:
+            return 0
+
+    def get_matrix(self):
+        return np.array([[self.probability() for _ in range(self.page_count)] for __ in range(self.page_count)])
+
+    def get_vector(self):
+        return np.array([1 / self.page_count for _ in range(self.page_count)])
+
+    def calculate_result(self):
+        response_vector = self.init_vector
+        for iteration in range(self.iterations):
+            response_vector = self.init_matrix @ response_vector
+        return response_vector
+
+    def __str__(self):
+        response = f"Init matrix: \n{self.init_matrix}\n Init vector: \n{self.init_vector}\n" \
+                   f"After {self.iterations} iterations vector of probability will be: \n{self.calculate_result()}"
+        return response
+
+
+print(str(PageRank()))
